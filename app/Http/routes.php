b25c41f04b73842d536d93950638c85a74c8bedf
Gerardo Paolo Davila Garcia <a20101616@pucp.pe>
2017-02-15 16:21:55 -0500

    reestableciendo proyecto[

---
+++ app/Http/routes.php
@@ -0,0 +1,113 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('about', function () {
+    return view('about');
+});
+
+Route::get('blog', function () {
+    return view('blog');
+});
+
+Route::get('blog_single', function () {
+    return view('blog_single');
+});
+
+Route::get('buy', function () {
+    return view('buy');
+});
+
+Route::get('buy_single', function () {
+    return view('buy_single');
+});
+
+Route::get('career', function () {
+    return view('career');
+});
+
+Route::get('contact', function () {
+    return view('contact');
+});
+
+Route::get('contact', function () {
+    return view('contact');
+});
+
+Route::get('dealers', function () {
+    return view('dealers');
+});
+
+Route::get('faqs', function () {
+    return view('faq');
+});
+
+Route::get('loan', function () {
+    return view('loan');
+});
+
+Route::get('loan_single', function () {
+    return view('loan_single');
+});
+
+
+Route::get('login', function () {
+    return view('login');
+});
+
+
+Route::get('mobile_app', function () {
+    return view('mobile_app');
+});
+
+
+Route::get('privacy', function () {
+    return view('privacy');
+});
+
+
+Route::get('register', function () {
+    return view('register');
+});
+
+
+Route::get('privacy', function () {
+    return view('privacy');
+});
+
+
+Route::get('single', function () {
+    return view('single');
+});
+
+
+Route::get('suggestion', function () {
+    return view('suggestion');
+});
+
+
+Route::get('terms', function () {
+    return view('terms');
+});
+
+Route::get('typo', function () {
+    return view('typo');
+});
+
+
+Route::get('facebook', 'Auth\AccountFBController@facebook_redirect');
+Route::get('callback', 'Auth\AccountFBController@handleProviderCallback');
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,94 @@
+@extends('layout.indexTemplate')
+
+@section('titulo')
+
+ 	 <div class="titulo">
+		<h1>NovoSpace</h1>
+
+		<div class="top-nav">
+	
+			<ul class = "right-icons">
+				<li><a  href="login"><i class="glyphicon glyphicon-user"> </i>Login</a></li>
+			</ul>
+
+			<ul class = "right-icons">
+				<li><a class="play-icon popup-with-zoom-anim" href="#small-dialog"><i class="glyphicon glyphicon-search"> </i> </a></li>
+			</ul>
+
+			<div class="nav-icon">
+				<div class="hero fa-navicon fa-2x nav_slide_button" id="hero">
+						<a href="#"><i class="glyphicon glyphicon-menu-hamburger"></i> </a>
+				</div>	
+			<!---pop-up-box---->
+				<link href="css/popuo-box.css" rel="stylesheet" type="text/css" media="all"/>
+				<script src="js/jquery.magnific-popup.js" type="text/javascript"></script>
+			<!---//pop-up-box---->
+				<div id="small-dialog" class="mfp-hide">
+					    <!----- tabs-box ---->
+				<div class="sap_tabs">	
+				     <div id="horizontalTab" style="display: block; width: 100%; margin: 0px;">
+						  <ul class="resp-tabs-list">
+						  	  <li class="resp-tab-item " aria-controls="tab_item-0" role="tab"><span>All Homes</span></li>
+							  <li class="resp-tab-item" aria-controls="tab_item-1" role="tab"><span>For Sale</span></li>
+							  <li class="resp-tab-item" aria-controls="tab_item-2" role="tab"><span>For Rent</span></li>
+							  <div class="clearfix"></div>
+						  </ul>				  	 
+						  <div class="resp-tabs-container">
+						  		<h2 class="resp-accordion resp-tab-active" role="tab" aria-controls="tab_item-0"><span class="resp-arrow"></span>All Homes</h2><div class="tab-1 resp-tab-content resp-tab-content-active" aria-labelledby="tab_item-0" style="display:block">
+								 	<div class="facts">
+									  	<div class="login">
+											<input type="text" value="Search Address, Neighborhood, City or Zip" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search Address, Neighborhood, City or Zip';}">		
+									 		<input type="submit" value="">
+									 	</div>        
+							        </div>
+						  		</div>
+							     <h2 class="resp-accordion" role="tab" aria-controls="tab_item-1"><span class="resp-arrow"></span>For Sale</h2><div class="tab-1 resp-tab-content" aria-labelledby="tab_item-1">
+									<div class="facts">									
+										<div class="login">
+											<input type="text" value="Search Address, Neighborhood, City or Zip" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search Address, Neighborhood, City or Zip';}">		
+									 		<input type="submit" value="">
+									 	</div> 
+							        </div>	
+								 </div>									
+							      <h2 class="resp-accordion" role="tab" aria-controls="tab_item-2"><span class="resp-arrow"></span>For Rent</h2><div class="tab-1 resp-tab-content" aria-labelledby="tab_item-2">
+									 <div class="facts">
+										<div class="login">
+											<input type="text" value="Search Address, Neighborhood, City or Zip" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search Address, Neighborhood, City or Zip';}">		
+									 		<input type="submit" value="">
+									 	</div> 
+							         </div>	
+							    </div>
+					      </div>
+					 </div>
+					 <script src="js/easyResponsiveTabs.js" type="text/javascript"></script>
+				    	<script type="text/javascript">
+						    $(document).ready(function () {
+						        $('#horizontalTab').easyResponsiveTabs({
+						            type: 'default', //Types: default, vertical, accordion           
+						            width: 'auto', //auto or any width like 600px
+						            fit: true   // 100% fit in a container
+						        });
+						    });
+			  			 </script>	
+				</div>
+				</div>
+				 <script>
+						$(document).ready(function() {
+						$('.popup-with-zoom-anim').magnificPopup({
+							type: 'inline',
+							fixedContentPos: false,
+							fixedBgPos: true,
+							overflowY: 'auto',
+							closeBtnInside: true,
+							preloader: false,
+							midClick: true,
+							removalDelay: 300,
+							mainClass: 'my-mfp-zoom-in'
+						});
+																						
+						});
+				</script>
+	</div>
+	 </div>
+@stop
+
