189a2364f424601f882f8fe871b1588c0f384e05
wingerlion <a20101616@pucp.pe>
2017-02-17 22:28:05 -0500

    xd

--- config/app.php
+++ config/app.php
@@ -203,7 +203,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Socialize' => Laravel\Socialite\Facades\Socialite::class,
+		'Form' => Collective\Html\FormFacade::class,
+		'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
